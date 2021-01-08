8f4c32f28a3f3dfa27e62af934a6e5636e736504
Diego <Diego@192.168.0.102>
2021-01-08 15:12:17 -0300

    este exercicio possui um método replaseAll() que retira/substitui caracteres

---
+++ src/main/java/com/mycompany/removeracentos/RemoveAcento.java
@@ -0,0 +1,35 @@
+/*
+ recebe uma string e remove todos os acentos
+ */
+package com.mycompany.removeracentos;
+
+import java.util.Scanner;
+
+
+public class RemoveAcento {
+
+    
+    @SuppressWarnings("UseOfSystemOutOrSystemErr")
+    public static void main(String[] args) {
+        
+        Scanner acento = new Scanner(System.in);
+        System.out.println("Digite a frase.");
+        String frase = acento.nextLine();
+        System.out.println(frase.replaceAll("[,.;:/~^´`!@#$%¨&*=+'|]", ""));
+        /* o método replaceAll realliza a retirada de caracters especiais (acentos)*/
+       
+        }
+  }
+    
+
+
+
+
+
+
+
+
+
+
+
+
